8cd3f1b9eae5dd2b1cec92de68d4eb2a912cd056
HiroX24 <124040742+HiroX24@users.noreply.github.com>
2025-11-05 16:18:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ def home():
 def test():
     return "Testing... <br> good <br> okay"
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environment.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
